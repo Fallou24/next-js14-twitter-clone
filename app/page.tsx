46a1e236ced47de2bbd6e8b64142d70638820adc
Fallou24 <falloundiaye0221@gmail.com>
2023-12-14 20:25:08 +0000

    Authentification

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>hello</div>
-  )
+    <div>
+      <UserButton />
+    </div>
+  );
 }
