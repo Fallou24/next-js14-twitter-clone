77ff7639064a67ebcb481eedb8c55647f04510cb
Fallou24 <falloundiaye0221@gmail.com>
2024-01-16 16:37:42 +0000

    Fix redirection

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,17 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks(.*)","/api/search"]
+  publicRoutes: ["/api/webhooks(.*)", "/api/search"],
+  afterAuth(auth, req, evt) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
